132748bc499f498a63e5bb159255947e2a222d86
ihomelab <55240929+ihomelab@users.noreply.github.com>
2024-03-26 14:16:25 +0100

    added correlator sources

---
+++ src/correlator/const.py
@@ -0,0 +1,7 @@
+PHASE_CHANGES = {
+    "inst_01": {"freezer": {"remove_start": "", "remove_end": "2018-09-20"}},
+    "inst_02": {"washing_machine": {"remove_start": "", "remove_end": "2018-09-04"}},
+    "inst_03": {
+        "fridge": {"remove_start": "", "remove_end": "2020-04-01"},
+    },
+}
